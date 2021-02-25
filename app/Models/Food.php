48a94df96fb0b7a74368783040472512b595fc60
Jorge Morales <JorgeMoralesMV@users.noreply.github.com>
2021-02-25 01:22:19 -0600

    Update Food.php

--- app/Models/Food.php
+++ app/Models/Food.php
@@ -68,6 +68,7 @@ class Food extends Model implements HasMedia
         'unit',
         'featured',
         'deliverable',
+        'food_status',
         'restaurant_id',
         'category_id'
     ];
@@ -88,6 +89,7 @@ class Food extends Model implements HasMedia
         'unit' => 'string',
         'featured' => 'boolean',
         'deliverable' => 'boolean',
+        'food_status' => 'boolean',
         'restaurant_id' => 'integer',
         'category_id' => 'integer'
     ];
